bb3107a2fc9c52ef6ab2253cbe090f53276366b5
jungminji <wjdalswl0215@gmail.com>
2025-03-14 05:28:57 +0900

    feat : todo 수정

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,17 +1,87 @@
 "use client";
 
-import { getTodos } from "@/service/todos";
-import { useSuspenseQuery } from "@tanstack/react-query";
-import React from "react";
+import { getTodos, updateTodo } from "@/service/todos";
+import { Todo } from "@/types/todos";
+import {
+  useMutation,
+  useQueryClient,
+  useSuspenseQuery,
+} from "@tanstack/react-query";
+import React, { useState } from "react";
+
+type UpdateTodoParams = {
+  title: string;
+  id: string;
+  completed: boolean;
+};
 
 export default function Todos() {
+  const queryClient = useQueryClient();
+
+  const [todoId, setTodoId] = useState<string | null>(null);
+  const [title, setTitle] = useState("");
+
   const { data } = useSuspenseQuery({ queryKey: ["todos"], queryFn: getTodos });
+
+  const updateMutation = useMutation({
+    mutationFn: (todo: Todo) => updateTodo(todo.title, todo.id, todo.completed),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["todos"] });
+
+      setTodoId(null);
+      setTitle("");
+    },
+  });
+
+  const handleUpdate = (todo: Todo) => {
+    if (title.trim() !== "" && title !== todo.title) {
+      updateMutation.mutate({
+        title: title,
+        id: todo.id,
+        completed: todo.completed,
+      });
+    } else {
+      setTodoId(null);
+      setTitle("");
+    }
+  };
+
+  const handleEdit = (todo: {
+    id: string;
+    title: string;
+    completed: boolean;
+  }) => {
+    setTodoId(todo.id);
+    setTitle(todo.title);
+  };
+
+  const handleCancelEdit = () => {
+    setTodoId(null);
+    setTitle("");
+  };
 
   return (
     <>
       <ul>
         {data?.map((todo) => (
-          <li key={todo.id}>{todo.title}</li>
+          <li key={todo.id}>
+            {todoId === todo.id ? (
+              <>
+                <input
+                  type="text"
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                />
+                <button onClick={() => handleUpdate(todo)}>저장</button>
+                <button onClick={handleCancelEdit}>취소</button>
+              </>
+            ) : (
+              <>
+                <span>{todo.title}</span>
+                <button onClick={() => handleEdit(todo)}>수정</button>
+              </>
+            )}
+          </li>
         ))}
       </ul>
     </>
--- src/service/todos.ts
+++ src/service/todos.ts
@@ -36,3 +36,49 @@ export const addTodo = async (todo: string) => {
     message: "성공",
   };
 };
+
+export const updateTodo = async (
+  todo: string,
+  id: string,
+  completed: boolean
+) => {
+  console.log("========== todos 수정 ==========");
+
+  const response = await fetch(`http://localhost:3001/todos/${id}`, {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      id,
+      title: todo,
+      completed,
+    }),
+  });
+
+  if (!response.ok) {
+    throw new Error("fail update todo");
+  }
+
+  return {
+    success: true,
+    message: "성공",
+  };
+};
+
+export const deleteTodo = async (
+  id: string
+): Promise<{ success: boolean; message: string }> => {
+  console.log("========== todos 삭제 ==========");
+
+  const response = await fetch(`http://localhost:3001/todos/${id}`, {
+    method: "DELETE",
+  });
+
+  if (!response.ok) {
+    throw new Error("fail delete todo");
+  }
+
+  return {
+    success: true,
+    message: "성공",
+  };
+};
