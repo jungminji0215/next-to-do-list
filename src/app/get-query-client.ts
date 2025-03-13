b185b0c1ac1ae811a814eedf96b8ed385c8d3053
jungminji <wjdalswl0215@gmail.com>
2025-03-14 02:19:34 +0900

    update : query client 수정

---
+++ src/app/get-query-client.ts
@@ -0,0 +1,23 @@
+// Since QueryClientProvider relies on useContext under the hood, we have to put 'use client' on top
+import { isServer, QueryClient } from "@tanstack/react-query";
+
+function makeQueryClient() {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 60 * 1000,
+      },
+    },
+  });
+}
+
+let browserQueryClient: QueryClient | undefined = undefined;
+
+export function getQueryClient() {
+  if (isServer) {
+    return makeQueryClient();
+  } else {
+    if (!browserQueryClient) browserQueryClient = makeQueryClient();
+    return browserQueryClient;
+  }
+}
